731abcf3191692a5c707cffbf3055e3bb4cb70b8
Rob Homewood <rob.homewood@googlemail.com>
2018-04-10 03:18:41 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Wordpress REST Migrator (WRM)
+
+WRM is a free and open source tool for Wordpress which helps you to export
+or migrate your Wordpress site using the Wordpress REST API.
---
+++ wrm.py
@@ -0,0 +1,17 @@
+""" Wordpress REST Migrator (WRM) Application.
+WRM is a free and open source tool for Wordpress which helps you to export
+or migrate your Wordpress site using the Wordpress REST API.
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2018, Robert J Homewood
+"""
+
